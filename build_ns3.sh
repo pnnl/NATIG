3c0cc5e2add80121f4dfafa4334a0902ae83d875
Oceane <obel@pnnl.gov>
2023-07-10 01:56:36 +0000

    backing up script to make ns3

--- build_ns3.sh
+++ build_ns3.sh
@@ -5,19 +5,31 @@ then
     rm -r /rd2c/test/
 fi
 
+if [ -d "/rd2c/ns-3-dev" ]
+then
+    rm -r /rd2c/ns-3-dev/
+fi
+
+if [ -d "/rd2c/ns-3-dev-git" ]
+then
+    rm -r /rd2c/ns-3-dev-git/
+fi
+
 cd ../../
-mkdir test
-cd test 
+#mkdir test
+#cd test 
 git clone https://github.com/nsnam/ns-3-dev-git.git
 mv ns-3-dev-git ns-3-dev
 cd ns-3-dev
 git checkout ns-3.35
-cp -r ../../PUSH/NATIG/patch/make.sh .
-cp -r ../../PUSH/NATIG/patch/helics-backup/ contrib/helics
-cp -r ../../PUSH/NATIG/patch/fncs/ src
-cp -r ../../PUSH/NATIG/patch/dnp3/ src
-cp -r ../../PUSH/NATIG/patch/applications/* src/applications/
-cp -r ../../PUSH/NATIG/patch/internet/* src/internet/
+cp -r ../PUSH/NATIG/patch/make.sh .
+cp -r ../PUSH/NATIG/patch/helics-backup/ contrib/helics
+cp -r ../PUSH/NATIG/patch/fncs/ src
+cp -r ../PUSH/NATIG/patch/dnp3/ src
+cp -r ../PUSH/NATIG/patch/applications/* src/applications/
+cp -r ../PUSH/NATIG/patch/internet/* src/internet/
+cp -r ../PUSH/NATIG/patch/lte/* src/lte/
+sudo ./make.sh
 if [ "$1" == "5G" ]; then
     echo "installing 5G"
     cd contrib
@@ -25,6 +37,6 @@ if [ "$1" == "5G" ]; then
     cd nr
     git checkout 5g-lena-v1.2.y
     cd ../../
-    cp -r ../../PUSH/NATIG/patch/nr/* contrib/nr/
+    cp -r ../PUSH/NATIG/patch/nr/* contrib/nr/
 fi
-sudo ./make.sh
+#sudo ./make.sh
