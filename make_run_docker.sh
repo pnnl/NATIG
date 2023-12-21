8994e54f29f46bf0f010182408ce770ef40a9670
purohitsumit <purohitsumit@gmail.com>
2023-12-21 11:45:11 -0800

    dont start it in 'remove' mode

--- make_run_docker.sh
+++ make_run_docker.sh
@@ -1,2 +1,2 @@
-docker build --network=host -f Dockerfile -t pnnl/natig:natigbase .
-docker run -it --rm --name=cps_clean pnnl/natig:natigbase bash
+docker build --network=host -f Dockerfile -t pnnl/natig:natigbase --no-cache .
+docker run -it --name=natigbase_container pnnl/natig:natigbase bash
