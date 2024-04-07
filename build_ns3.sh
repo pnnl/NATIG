57285e86fbe7ae44e7f085f21ef5c2cb72b32ff6
Oceane Bel <obel@pnnl.gov>
2024-04-07 01:05:06 +0000

    Removed dependencies with internet module

--- build_ns3.sh
+++ build_ns3.sh
@@ -25,22 +25,14 @@ git checkout ns-3.35
 cd /rd2c/PUSH/NATIG
 ./build_helics.sh
 cd /rd2c/ns-3-dev
-#cp -r ../PUSH/NATIG/patch/make.sh .
-#cp -r ../PUSH/NATIG/patch/helics-backup/ contrib/helics
-#cp -r ../PUSH/NATIG/patch/fncs/ src
-#cp -r ../PUSH/NATIG/patch/dnp3/ src
-#cp -r ../PUSH/NATIG/patch/applications/* src/applications/
-#cp -r ../PUSH/NATIG/patch/internet/* src/internet/
-#cp -r ../PUSH/NATIG/patch/lte/* src/lte/
-#cp -r ../PUSH/NATIG/patch/point-to-point-layout/* src/point-to-point-layout/
 cp -r ../PUSH/NATIG/RC/code/make.sh .
-cp -r ../PUSH/NATIG/RC/code/fncs/ src
 cp -r ../PUSH/NATIG/RC/code/dnp3/ src
-cp -r ../PUSH/NATIG/RC/code/applications/* src/applications/
-cp -r ../PUSH/NATIG/RC/code/internet/* src/internet/
+cp -r ../PUSH/NATIG/RC/code/dnp3/model/dnp3-application-Docker.cc src/dnp3/model/dnp3-application.cc
+cp -r ../PUSH/NATIG/RC/code/internet/ipv4-l3-protocol-MIM.* src/internet/model/
+cp -r ../PUSH/NATIG/RC/code/internet/internet-stack-helper-MIM.* src/internet/helper/
+cp -r ../PUSH/NATIG/RC/code/internet/wscript src/internet/
 cp -r ../PUSH/NATIG/RC/code/lte/* src/lte/
 cp -r ../PUSH/NATIG/RC/code/point-to-point-layout/* src/point-to-point-layout/
-cp -r ../PUSH/NATIG/RC/code/dnp3/model/dnp3-application-Docker.cc src/dnp3/model/dnp3-application.cc 
 sudo ./make.sh $2
 if [ "$1" == "5G" ]; then
     echo "installing 5G"
