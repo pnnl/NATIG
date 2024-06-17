a8b0b54c0d2ed4a2daeebb47780c0854ae401333
Oceane <obel@pnnl.gov>
2024-06-17 11:35:53 -0700

    Update to ns3 make file

--- RC/code/make.sh
+++ RC/code/make.sh
@@ -2,6 +2,6 @@
 
 LDFLAGS="-ljsoncpp -L$1/include/json/"
 #./waf clean
-./waf configure --prefix=$1 --with-helics=$1  --disable-fncs --with-czmq=$1 --with-zmq=$1 --disable-werror --enable-examples --enable-tests --enable-mpi 
+./waf configure --prefix=$1 --with-helics=$1  --with-zmq=$1 --disable-werror --enable-examples --enable-tests --enable-mpi 
 ./waf build
 ./waf install
