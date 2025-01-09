0bf10cdf63833b9ed9d54530b94cda8c175c7ee4
Oceane <obel@pnnl.gov>
2025-01-09 09:49:49 -0800

    update workstation

--- update_workstation.sh
+++ update_workstation.sh
@@ -42,10 +42,13 @@ cp -r RC/code/internet/* ${RD2C}/ns-3-dev/src/internet/
 cp -r RC/code/lte/* ${RD2C}/ns-3-dev/src/lte/ 
 cp -r RC/code/gridlabd/* ${RD2C}/gridlab-d/tape_file/ 
 cp -r RC/code/trigger.player ${RD2C}/integration/control/
-cp -r /rd2c/PUSH/NATIG/RC/code/helics/helics-helper* /rd2c/ns-3-dev/contrib/helics/helper/
-cp -r /rd2c/PUSH/NATIG/RC/code/helics/dnp3-application-helper-new.* /rd2c/ns-3-dev/contrib/helics/helper/
-cp -r /rd2c/PUSH/NATIG/RC/code/helics/dnp3-application-new* /rd2c/ns-3-dev/contrib/helics/model/
-cp -r /rd2c/PUSH/NATIG/RC/code/helics/wscript /rd2c/ns-3-dev/contrib/helics/
+cd ${RD2C}/PUSH/NATIG
+./build_helics.sh
+cd ${RD2C}/ns-3-dev
+cp -r ${RD2C}/PUSH/NATIG/RC/code/helics/helics-helper* ${RD2C}/ns-3-dev/contrib/helics/helper/
+cp -r ${RD2C}/PUSH/NATIG/RC/code/helics/dnp3-application-helper-new.* ${RD2C}/ns-3-dev/contrib/helics/helper/
+cp -r ${RD2C}/PUSH/NATIG/RC/code/helics/dnp3-application-new* ${RD2C}/ns-3-dev/contrib/helics/model/
+cp -r ${RD2C}/PUSH/NATIG/RC/code/helics/wscript ${RD2C}/ns-3-dev/contrib/helics/
 
 
 cd ${RD2C}/gridlab-d/third_party/xerces-c-3.2.0 
